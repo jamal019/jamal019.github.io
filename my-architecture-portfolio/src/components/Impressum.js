d13f3bbbe8755c3dd6f43b9bde6f856433bb9dc5
jamal019 <97345834+jamal019@users.noreply.github.com>
2024-01-03 19:03:46 +0100

    Verbindung mit Github Pages

--- my-architecture-portfolio/src/components/Impressum.js
+++ my-architecture-portfolio/src/components/Impressum.js
@@ -1,12 +1,56 @@
 import React from 'react';
+import './Datenschutz.css'; 
 
 function Impressum() {
   return (
-    <div>
+    <div className="container">
       <h2>Impressum</h2>
-      {/* Impressum und Datenschutzerklärung */}
-    </div>
-  );
+      <h3>Angaben gemäß § 5 TMG:</h3>
+      <p>Melike Koc<br />
+      Seidelbastweg 115<br />
+      12357 Berlin<br />
+      Architektenkammer Berlin Nr. 77777<br />
+      </p>
+
+      <h3>Kontaktaufnahme:</h3>
+      <p>Telefon: +49 1575 8864333<br />
+      E-Mail: <a href="melikekoc10@hotmail.com">melikekoc10@hotmail.com</a></p>
+
+
+      <h3>Umsatzsteuer-ID:</h3>
+      <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:</p>
+      <p>DE Ihre USt-ID-Nummer</p>
+
+      <h3>Haftungsausschluss – Disclaimer:</h3>
+      <h4>Haftung für Inhalte</h4>
+      <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen...</p>
+      {/* Fügen Sie hier den vollständigen Haftungsausschluss Text ein. */}
+
+      <h4>Haftungsbeschränkung für externe Links</h4>
+      <p>Unsere Website enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben...</p>
+      {/* Fügen Sie hier den vollständigen Haftungsbeschränkungstext für externe Links ein. */}
+
+      <h4>Urheberrecht</h4>
+      <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht...</p>
+      {/* Fügen Sie hier den vollständigen Urheberrechtstext ein. */}
+
+      <p>Sollten Sie auf unserer Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte unverzüglich entfernen.</p>
+
+<p>Dieses Impressum gilt auch für alle Online-Präsenzen unserer Firma, wie zum Beispiel Social-Media-Profile.</p>
+
+<h3>Datenschutz</h3>
+<p>Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich...</p>
+{/* Fügen Sie hier den vollständigen Datenschutztext ein, falls nötig. */}
+
+<h3>Widerspruch Werbe-Mails</h3>
+<p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit widersprochen...</p>
+
+<p>Bitte ersetzen Sie alle Platzhaltertexte durch die spezifischen Informationen Ihrer Firma und passen Sie die Inhalte den tatsächlichen Gegebenheiten an. Dieses Impressum wurde zur Verfügung gestellt von jurarat.de.</p>
+
+<p>Letzte Aktualisierung: 03.01.2024</p>
+</div>
+);
 }
 
 export default Impressum;
+
--- my-architecture-portfolio/src/components/Kontakt.js
+++ my-architecture-portfolio/src/components/Kontakt.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import './Datenschutz.css'; 
 
 function Kontakt() {
   return (
-    <div>
+    <div className="container">
       <h2>Kontakt</h2>
-      {/* Kontaktformular */}
-      <form>
-        {/* Formularfelder */}
-      </form>
-    </div>
-  );
+      <h3>Anschrift:</h3>
+      <p>Melike Koc<br />
+      Seidelbastweg 115<br />
+      12357 Berlin<br /></p>
+
+      <h3>Kontaktdaten:</h3>
+      <p>Telefon: +49 1575 8864333<br />
+      E-Mail: <a href="melikekoc10@hotmail.com">melikekoc10@hotmail.com</a></p>
+
+</div>
+);
 }
 
 export default Kontakt;
+
