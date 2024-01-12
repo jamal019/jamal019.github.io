37345603b824604648da9b8fa0d3db60c69bd3a8
jamal019 <97345834+jamal019@users.noreply.github.com>
2024-01-12 21:45:56 +0100

    Handyversion Test

--- my-architecture-portfolio/src/components/Navbar.js
+++ my-architecture-portfolio/src/components/Navbar.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css'; // Stellen Sie sicher, dass die CSS-Datei existiert
+import './Navbar.css';
 
 function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <NavLink to="/" className="navbar-logo">
-        <img src={process.env.PUBLIC_URL + '/testlogo.png'} alt="Logo" height="60" /> {/* Höhe des Logos anpassen */}
+          <img src={process.env.PUBLIC_URL + '/testlogo.png'} alt="Logo" height="60" />
         </NavLink>
-        <div className="navbar-menu">
+        <button
+          className="hamburger"
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
+        >
+          {/* Icon für Hamburger-Menü */}
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+        </button>
+        <div className={`navbar-menu ${isNavExpanded ? "expanded" : ""}`}>
           <NavLink to="/" className="navbar-item" activeClassName="active">Home</NavLink>
           <NavLink to="/projekte" className="navbar-item" activeClassName="active">Projekte</NavLink>
           <NavLink to="/about" className="navbar-item" activeClassName="active">Über mich</NavLink>
